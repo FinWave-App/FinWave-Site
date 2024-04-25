65c349278f91f1ff7ddf16f0a68526301b341f3b
isKonstantin <me@knst.su>
2024-04-25 18:10:16 +0300

    Added new SEO meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,14 @@ useSeoMeta({
   title: 'FinWave',
   ogTitle: 'FinWave',
   description: 'A convenient open source service for financial accounting',
-  ogDescription: 'A convenient open source service for financial accounting'
+  ogDescription: 'A convenient open source service for financial accounting',
+
+  ogImage: '/logo.png',
+  icon: '/logo.png',
+  ogUrl: 'https://finwave.app',
+  twitterTitle: 'FinWave',
+  twitterDescription: 'A convenient open source service for financial accounting',
+  twitterImage: '/logo.png'
 })
 
 </script>
